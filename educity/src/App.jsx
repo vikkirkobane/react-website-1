ea5ca54d746f651e1737cf3c0868b59b2eeaed76
victorchogo37 <32248021-victorchogo37@users.noreply.replit.com>
2024-03-31 22:27:56 +0000

    Initial commit

---
+++ educity/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Hero from './Components/Hero/Hero.jsx'
+import Programs from './Components/Programs/Programs.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Programs/>
+      </div>
+      
+    </div>
+  )
+  
+}
+
+export default App
